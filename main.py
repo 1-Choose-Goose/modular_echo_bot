860503b6924cecb4dfa5354877cbd0eaba8bec5a
Choose_Goose <gufsinchelyabinsk@yandex.ru>
2025-10-30 18:02:41 +0500

    Точка входа main.py из урока про роутеры

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello")
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+    # Задаём базовую конфигурацию логирования
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
